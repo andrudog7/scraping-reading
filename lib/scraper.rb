07f18687dd77258a6a1137fe8f3ba7b49da2bb29
--add <andrewksmoker@gmail.com>
2020-11-03 17:17:01 -0500

    commit tree 5972dc9eaa770e019e1eeee722cdd640ca5e001c

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+doc.css(".headling-260IBN").text
+courses = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")
+courses.each do |course|
+    puts course.text.strip
+end
+binding.pry
+x = 0
